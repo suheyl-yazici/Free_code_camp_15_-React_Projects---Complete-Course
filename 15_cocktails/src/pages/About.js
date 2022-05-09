2660e1b04c0e242e88ce7f9e0a81c4968d29a0c8
suheyl-yazici <h.suheylyazici@gmail.com>
2022-05-09 13:19:02 +0300

    15.proje tamamlandı

---
+++ 15_cocktails/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Pariatur commodo consequat labore sunt aliquip laboris id culpa culpa.
+        Sint ex nulla quis voluptate commodo dolor proident cillum minim labore.
+        Reprehenderit non reprehenderit ullamco ea commodo. Dolor aliqua
+        consequat consectetur commodo ut adipisicing ut id aliqua Lorem dolore.
+      </p>
+    </section>
+  );
+};
+
+export default About;
